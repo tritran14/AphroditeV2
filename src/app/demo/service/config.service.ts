671b5d5a71d8890d42263c71eda637450031dcb0
Tri <tri.tranvan@axonactive.com>
2022-12-01 13:06:37 +0700

    update amat url

--- src/app/demo/service/config.service.ts
+++ src/app/demo/service/config.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
     // AMAT_URL = 'http://localhost:5050';
-    AMAT_URL = 'http://103.162.20.182:5050';
+    AMAT_URL = 'https://103.162.20.182:5051';
     HERMES_URL = 'http://localhost:8082';
     constructor() {}
 }
